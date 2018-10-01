124155eabc08db5bcbe8dba9f7c5a3d92ece47cf
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 17:40:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def name=(dog_name)
+    @this_dogs_name = dog_name
   end
 
   def name
@@ -9,6 +9,6 @@ class Dog
   end
 
 
-  
+
 
 end
